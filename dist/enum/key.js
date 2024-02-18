40fcf07d46df601a96a190b0cf8cb7b0d1747cf8
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-19 09:55:22 +1300

    Add logger functionality and update .gitignore

---
+++ dist/enum/key.js
@@ -0,0 +1,18 @@
+var Key;
+(function (Key) {
+    // @error keys
+    Key["Connected"] = "CONNECTED \uD83D\uDFE2";
+    Key["NotConnected"] = "NOT CONNECTED \uD83D\uDD34";
+    Key["Environment"] = " ENVIRONMENT: ";
+    Key["Production"] = "production";
+    Key["Development"] = "development";
+    // @logger - req
+    Key["ReqMethod"] = " Request Method: ";
+    Key["ReqURL"] = " Request URL: ";
+    Key["ReqTime"] = " Request Time: ";
+    // @logger - server
+    Key["ServerPort"] = " SERVER PORT: ";
+    Key["ServerAPIVersion"] = " API VERSION: ";
+    Key["ServerStatus"] = " SERVER STATUS: ";
+})(Key || (Key = {}));
+export default Key;
